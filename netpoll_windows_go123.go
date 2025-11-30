413e892ac443ad795309d2193648d4ab960e04c5
wwqgtxx <wwqgtxx@gmail.com>
2025-12-01 00:20:39 +0800

    share some code

--- netpoll_windows_go123.go
+++ netpoll_windows_go123.go
@@ -4,9 +4,6 @@ package tfo
 
 import (
 	"errors"
-	"net"
-	"os"
-	"sync"
 	"syscall"
 	"unsafe"
 
@@ -160,8 +157,6 @@ func init() {
 	checkSetFileCompletionNotificationModes()
 }
 
-var serverInit sync.Once
-
 func (fd *pFD) Init(net string, pollable bool) (string, error) {
 	switch net {
 	case "file", "dir":
@@ -230,104 +225,4 @@ func (fd *pFD) Init(net string, pollable bool) (string, error) {
 	fd.rop.runtimeCtx = fd.pd.runtimeCtx
 	fd.wop.runtimeCtx = fd.pd.runtimeCtx
 	return "", nil
-}
-
-// Error values returned by runtime_pollReset and runtime_pollWait.
-// These must match the values in runtime/netpoll.go.
-const (
-	pollNoError        = 0
-	pollErrClosing     = 1
-	pollErrTimeout     = 2
-	pollErrNotPollable = 3
-)
-
-func convertErr(res int, isFile bool) error {
-	switch res {
-	case pollNoError:
-		return nil
-	case pollErrClosing:
-		return errClosing(isFile)
-	case pollErrTimeout:
-		return os.ErrDeadlineExceeded
-	case pollErrNotPollable:
-		return ErrNotPollable
-	}
-	println("unreachable: ", res)
-	panic("unreachable")
-}
-
-// ErrNotPollable is returned when the file or socket is not suitable
-// for event notification.
-var ErrNotPollable = errors.New("not pollable")
-
-// ErrFileClosing is returned when a file descriptor is used after it
-// has been closed.
-var ErrFileClosing = errors.New("use of closed file")
-
-// ErrNetClosing is returned when a network descriptor is used after
-// it has been closed.
-var ErrNetClosing = net.ErrClosed
-
-// ErrDeadlineExceeded is returned for an expired deadline.
-var ErrDeadlineExceeded error = os.ErrDeadlineExceeded
-
-// Return the appropriate closing error based on isFile.
-func errClosing(isFile bool) error {
-	if isFile {
-		return os.ErrClosed
-	}
-	return ErrNetClosing
-}
-
-//go:linkname runtime_pollServerInit internal/poll.runtime_pollServerInit
-func runtime_pollServerInit()
-
-//go:linkname runtime_pollOpen internal/poll.runtime_pollOpen
-func runtime_pollOpen(fd uintptr) (uintptr, int)
-
-//go:linkname runtime_pollWait internal/poll.runtime_pollWait
-func runtime_pollWait(ctx uintptr, mode int) int
-
-//go:linkname runtime_pollWaitCanceled internal/poll.runtime_pollWaitCanceled
-func runtime_pollWaitCanceled(ctx uintptr, mode int)
-
-//go:linkname runtime_pollReset internal/poll.runtime_pollReset
-func runtime_pollReset(ctx uintptr, mode int) int
-
-// Stay in sync with pollDesc in src/internal/poll/fd_poll_runtime.go
-type pollDesc struct {
-	runtimeCtx uintptr
-}
-
-func (pd *pollDesc) init(fd *pFD) error {
-	serverInit.Do(runtime_pollServerInit)
-	ctx, errno := runtime_pollOpen(uintptr(fd.Sysfd))
-	if errno != 0 {
-		return syscall.Errno(errno)
-	}
-	pd.runtimeCtx = ctx
-	return nil
-}
-
-func (pd *pollDesc) prepare(mode int, isFile bool) error {
-	if pd.runtimeCtx == 0 {
-		return nil
-	}
-	res := runtime_pollReset(pd.runtimeCtx, mode)
-	return convertErr(res, isFile)
-}
-
-func (pd *pollDesc) wait(mode int, isFile bool) error {
-	if pd.runtimeCtx == 0 {
-		return errors.New("waiting for unsupported file type")
-	}
-	res := runtime_pollWait(pd.runtimeCtx, mode)
-	return convertErr(res, isFile)
-}
-
-func (pd *pollDesc) waitCanceled(mode int) {
-	if pd.runtimeCtx == 0 {
-		return
-	}
-	runtime_pollWaitCanceled(pd.runtimeCtx, mode)
 }
